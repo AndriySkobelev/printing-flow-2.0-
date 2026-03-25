ca17a81fe968cf2446c893541cfcedea76ba9d81
Andriy Skobelev <AndriySkobelev@gmail.com>
2026-03-25 16:26:59 +0200

    save test

--- convex/auth.ts
+++ convex/auth.ts
@@ -1,6 +1,29 @@
 import { convexAuth } from "@convex-dev/auth/server";
 import Google from "@auth/core/providers/google";
+import { mutation, internalMutation, query } from "./_generated/server";
 
 export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
   providers: [Google],
 });
+
+export const authMutation = query({
+  handler: async (ctx) => {
+    const identity = await ctx.auth.getUserIdentity();
+    if (!identity) {
+      throw new Error("Unauthenticated call to mutation");
+    }
+    console.log('user', identity)
+    return null;
+  }
+})
+
+export const authMutation2 = internalMutation({
+  handler: async (ctx) => {
+    const identity = await ctx.auth.getUserIdentity();
+    if (!identity) {
+      throw new Error("Unauthenticated call to mutation");
+    }
+    console.log('user', identity)
+    return null;
+  }
+})
--- src/routes/app/route.tsx
+++ src/routes/app/route.tsx
@@ -1,7 +1,13 @@
 import { createFileRoute, Outlet } from '@tanstack/react-router'
 import Header from '@/components/Header'
+import { api } from 'convex/_generated/api'
+import { convexQuery } from '@convex-dev/react-query'
+
 export const Route = createFileRoute('/app')({
   component: RouteComponent,
+  beforeLoad: async ({ context: { queryClient} }) => {
+    const some = await queryClient.ensureQueryData(convexQuery(api.auth.authMutation))
+  }
 })
 
 function RouteComponent() {
